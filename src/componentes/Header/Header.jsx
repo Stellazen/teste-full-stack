2c0b00c97538a3fc7727e5242c85ef7a513ccc57
StellaZen <stellazen.xd@gmail.com>
2023-09-27 16:58:22 -0300

    style: css da página de login

---
+++ src/componentes/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+function Header(){
+    return(
+        <>
+        <section className={styles.background}></section>
+        </>
+    )
+}
+
+export default Header;
