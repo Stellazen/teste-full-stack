569ba03ef349cfdd3ee4c4d393d57a6e5aeb857c
StellaZen <stellazen.xd@gmail.com>
2023-10-01 16:43:41 -0300

    refactor: movendo gitConfig

--- src/servicesFirebase/firebaseAuth.js
+++ src/servicesFirebase/firebaseAuth.js
@@ -9,7 +9,15 @@ import {
     updateProfile,
     onAuthStateChanged,
   } from 'firebase/auth';
-  import { firebaseConfig } from './firebaseconfig';
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyDk-k-vauTqICPVM9lSDClg5T0L2KyE6xo",
+    authDomain: "aquidev-5f229.firebaseapp.com",
+    projectId: "aquidev-5f229",
+    storageBucket: "aquidev-5f229.appspot.com",
+    messagingSenderId: "83788541614",
+    appId: "1:83788541614:web:dd7baec5a5793c1f26473f"
+  };
 
   const app = initializeApp(firebaseConfig);
   export const auth = getAuth(app);
